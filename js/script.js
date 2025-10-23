bbb7d9524b9315e46d8efb3f320152dfb2353f9e
Yuliia Nykanorova <juliyasos@ukr.net>
2025-10-23 15:45:48 +0300

    added functions for deleting tasks and some other

--- js/script.js
+++ js/script.js
@@ -1,47 +1,86 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function () {
-    //const btnAdd = document.querySelector("#todo-add-button");
     const input = document.querySelector("#todo-input");
     const form = document.querySelector("#todo-form");
     const list = document.querySelector("#todo-list");
+    
+    let tasks = [];
 
-    const notes = [];
+    list.addEventListener("click", (e) => {
+        const liElement = e.target.closest("li");
+        const btn = e.target.closest("button");
+        const text = e.target.classList.contains("span-text");
+        
+        if (!liElement) {
+            return;
+        }
 
-    function addNote (text, date) {
-        const newNote = document.createElement("li");
-        const newNoteDate = document.createElement("span");
-        const newNoteText = document.createElement("span");
+        const taskId = liElement.dataset.id;
+
+        if (text) {
+            const task = tasks.find((task) => task.id === taskId);
+            task.completed = !task.completed;
+            renderTasks();
+        }
+
+        if (btn) {
+            tasks = tasks.filter((task) => {
+                return task.id !== String(taskId);
+            });
+            renderTasks();
+        }
+    })
+
+
+    const noTasks = document.createElement("p");
+    noTasks.textContent = "No tasks yet...";
+
+    renderTasks();
+
+    function renderTasks () {
+        console.log("render");
+        list.innerHTML = "";
+
+        if(tasks.length === 0) {
+            return list.append(noTasks);
+        }
+
+        tasks.forEach((task) => {
+            addTask(task);
+        })
+    }
+
+    function addTask (newTask) {
+        const newTaskContainer = document.createElement("li");
+        newTaskContainer.dataset.id = newTask.id;
+        const newTaskDate = document.createElement("span");
+        const newTaskText = document.createElement("span");
+        newTaskText.classList.add("span-text");
         const btnDel = document.createElement("button");
-        newNoteText.textContent = text;
-        newNoteDate.textContent = date;
+        newTaskText.textContent = newTask.text;
+        newTaskDate.textContent = newTask.id;
         btnDel.textContent = "Delete";
         btnDel.classList.add("delete-button");
-        newNote.append(newNoteDate, newNoteText, btnDel);
-        list.append(newNote);
+        newTaskContainer.append(newTaskDate, newTaskText, btnDel);
+        list.append(newTaskContainer);
 
-        console.log(notes);
-
-        btnDel.addEventListener("click", () => {
-            newNote.remove();
-        });
-
-        newNoteText.addEventListener("click", () => {
-            newNoteText.classList.toggle("done");
-        })
+        if (newTask.completed) {
+            newTaskText.classList.add("done");
+        }
     }
 
     form.addEventListener("submit", (e) => {
         e.preventDefault();
         const id = Date.now();
         const date = new Date(id);
-        const note = {
-            id: date.toLocaleDateString("uk-UA"),
+        const task = {
+            id: date.toLocaleString("uk-UA"),
             text: input.value,
             completed: false,
         }
-        notes.push(note);
-        addNote(note.text, note.id);
+        tasks.push(task);
+        renderTasks();
         form.reset();
     })
 });
